f752d9dda359eef103fe8955233229364684fd65
Parzival <62294866+GitReboot@users.noreply.github.com>
2025-12-06 21:59:03 -0500

    optimized day 2 part 1

--- 2025/Day2/Day2.py
+++ 2025/Day2/Day2.py
@@ -2,14 +2,21 @@ total = 0
 
 with open('Day2.txt') as f:
     for x in f.read().split(","):
-        start = x.split("-")[0]
-        end = x.split("-")[1]
-        while int(start) != int(end) + 1:
-            if len(start) % 2 == 0:
-                fh = start[0:int((len(start)/2))]
-                sh = start[int((len(start)/2)):len(start)]
-                if fh == sh:
-                    total += int(fh+sh)
-            start = str(int(start) + 1)
+        start, end = map(int, x.split("-"))
+        start_len = len(str(start))
+        end_len = len(str(end))
+
+        for length in range(start_len, end_len + 1):
+            if length % 2 != 0:
+                continue
+
+            half_len = length // 2
+            fh = 10**(half_len - 1)
+            sh = (10**half_len) - 1
+            for y in range(fh, sh + 1):
+                guess = y * (10**half_len) + y
+
+                if start <= guess <= end:
+                    total += guess
 
 print(total)
